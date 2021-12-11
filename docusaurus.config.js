b46ab4af98d52e9546294cd50936989da2e2ec6d
微凉 <927625802@qq.com>
2021-12-11 14:56:27 +0800

    local search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -125,6 +125,23 @@ const config = {
     defaultLocale: "zh-CN",
     locales: ["zh-CN", "en"],
   },
+
+  plugins: [
+    // ... Your other plugins.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        language: ["en", "zh"],
+        // ```
+        // When applying `zh` in language, please install `nodejieba` in your project.
+      },
+    ],
+  ],
 };
 
 module.exports = config;
